2846f91597474de872cc2c4103046df9163ed21a
fitcodding <126243168+coding-ustad@users.noreply.github.com>
2024-03-02 22:49:25 +0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   server :{
     proxy: {
       '/api' :{
-        target: 'https://mern-blog-app-pi.vercel.app/',
+        target: 'http://localhost:3000',
         secure : false,
       },
     },
